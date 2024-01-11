dda68b6078cec107c75993367662b30921b6cd03
KowerKoint <kowerkoint@gmail.com>
2024-01-11 13:51:02 +0900

    init

---
+++ template/main.cpp
@@ -0,0 +1,14 @@
+#include "KowerKoint/base.hpp"
+
+void solve(){
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << fixed << setprecision(10);
+    int t = 1;
+    // cin >> t;
+    for(int case_id = 1; case_id <= t; case_id++) solve();
+    return 0;
+}
